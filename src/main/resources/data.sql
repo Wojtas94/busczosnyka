950ebabfeb554082bb48f563445089838690d736
Wojtek Misiaszek <wojciechmisiaszek94@gmail.com>
2020-02-10 19:49:13 +0100

    pom mod

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO user (id, name, surname, join_date) VALUES (1, 'Bartek', 'Czosnyka',  '2020-01-22');
-INSERT INTO user_credentials (id, login, password, email, role, is_password_changed, user_id) VALUES (1, 'Bartek', 'password1',  'bartek@gmail.com', 'ADMIN', 'TRUE', 0);
+INSERT INTO "user" ( name, surname, join_date) VALUES ( 'Bartek', 'Czosnyka',  '2020-01-22');
+INSERT INTO user_credentials ( login, password, email, role, is_password_changed, user_id) VALUES ('Bartek', 'password1',  'bartek@gmail.com', 'ADMIN', 'TRUE', 0);
 
