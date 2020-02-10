9e026b1c4dc90897dce7fc464e3c83e2c5ab64f3
Wojtek Misiaszek <wojciechmisiaszek94@gmail.com>
2020-02-10 19:06:38 +0100

    pom mod

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into bus.user(name, surname, join_date) values ('Bartek', 'Czosnyka',  '2020-01-22');
-insert into bus.user_credentials(login, password, email, role, is_password_changed, user) values ('Bartek', 'password1',  'bartek@gmail.com', 'ADMIN', 'TRUE', 1);
+insert into user(name, surname, join_date) values ('Bartek', 'Czosnyka',  '2020-01-22');
+insert into user_credentials(login, password, email, role, is_password_changed, user) values ('Bartek', 'password1',  'bartek@gmail.com', 'ADMIN', 'TRUE', 1);
 
